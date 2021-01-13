e32479f931b5547854975f40d7111a7c43a1355e
devanglefish <jaebeommun@gmail.com>
2021-01-13 16:24:30 +0900

    예외 페이징 처리

--- jblog03/src/main/java/com/bitacademy/jblog/exception/GlobalExceptionHandler.java
+++ jblog03/src/main/java/com/bitacademy/jblog/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.io.StringWriter;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,11 +14,11 @@ public class GlobalExceptionHandler {
 	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
-	public String handleException(Exception e) {
+	public String handleException(Exception e, Model model) {
 		StringWriter errors = new StringWriter(); // 버퍼
 		e.printStackTrace(new PrintWriter(errors));
 		LOGGER.error(errors.toString());
 		
-		return "main/index";
+		return "exception/exception";
 	}
 }
